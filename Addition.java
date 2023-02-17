7ca59477a0e67243f750569518589ab3887b777c
VANSHIKA206 <69639037+VANSHIKA206@users.noreply.github.com>
2023-02-17 19:03:49 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+    public static void main(String args[])
+    {
+        int num1 = 7;
+        int num2 = 10;
+
+        int result = num1 +num2;
+        System.out.println(result);
+
+    }
+}
---
+++ Division.java
@@ -0,0 +1,11 @@
+public class Division {
+    public static void main(String args[])
+    {
+        int num1 = 20;
+        int num2 = 2;
+
+        int result = (num1/num2);
+        System.out.println(result);
+    }
+    
+}
